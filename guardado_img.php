d069a30971fd39a59eb5d577864285845ba72f10
criss300v1 <crisstrabajos004@gmail.com>
2023-05-10 11:32:27 -0500

    sirve el validar de guardado_img.php

--- guardado_img.php
+++ guardado_img.php
@@ -3,18 +3,17 @@
   
 
   if (isset($_FILES['imagen']) && $_FILES['imagen']['error'] === UPLOAD_ERR_OK) {
-
+    $id_usuario = $_POST['id'];
     $nombre = $_POST['nombre'];
     $descripcion = $_POST['descripcion'];
     $tipo = $_FILES['imagen']['type'];
     $datos = file_get_contents($_FILES['imagen']['tmp_name']);
-    $remember = isset($_POST["remember"]);
     
 
-    $query = "INSERT INTO imagenes (nombre, descripcion, imagen) VALUES (?, ?, ?)";
-    $stmt = mysqli_prepare($conexion, $query);
-    mysqli_stmt_bind_param($stmt, "sss", $nombre, $descripcion, $datos);
-    mysqli_stmt_execute($stmt);
+    $query = "INSERT INTO imagenes VALUES ('','$id_usuario', '$nombre', '$descripcion','$tipo')";
+    $stmt = mysqli_query($conexion, $query);
+    //mysqli_stmt_bind_param($stmt, "sss", $nombre, $descripcion, $datos, $id_usuario);
+    //mysqli_stmt_execute($stmt);
 
     if($stmt){
         header('location:indice.html');
