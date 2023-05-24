2327ab59e8c9df7dffc2334bb1b7de296f1c567a
jeanca123 <53807105+jeanca123@users.noreply.github.com>
2023-05-24 16:37:22 -0500

    PHP.se añadio el Mostrar para cambio

--- mostrar.php
+++ mostrar.php
@@ -1,48 +1,88 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
 </head>
+
 <body>
-    <center> 
-        <table border="3">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>nombre</th>
-                    <th>descripcion</th>
-                    <th>imagen</th>
-                    <th>operaciones</th>
-                    <th></th>
-                </tr>
-            <thead>
-                <tbody>
-                    <?php
-                    include("conexion.php");
-                    $conexion = mysqli_connect("localhost", "root", "", "software-ing");
-
-                    $query = "SELECT * FROM imagenes";
-                    $resultado = $conexion->query($query);
-                    while($row = $resultado->fetch_assoc()){
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-12">
+                <?php
+                    if (isset($_REQUEST['guardar'])) {
+                           if(isset($_FILES['imagen']['name'])){
+                            $tipoArchivo = $_FILES['imagen']['type'];
+                            $nombreArchivo = $_FILES['imagen']['name'];
+                            $tamanoArchivo = $_FILES['imagen']['size'];
+                            $imagenSubida = fopen($_FILES['imagen']['tmp_name'],'r');
+                            $binariosImagen = fread($imagenSubida,$tamanoArchivo);
+                            include_once "conexion.php";
+                            $conexion = mysqli_connect("localhost", "root", "", "software-ing");
+                            $binariosImagen = mysqli_escape_string($conexion,$binariosImagen);
+                            $query="INSERT INTO imagenes2 (nombre,             imagen,            tipo) values
+                                                          ('".$nombreArchivo."','".$binariosImagen."','".$tipoArchivo."')";
+                            $res = mysqli_query($conexion,$query);
+                            if($res){
                     ?>
-                        <tr>
-                            <td><?php echo $row['id'];?></td>
-                            <td><?php echo $row['nombre'];?></td>
-                            <td><?php echo $row['descripcion'];?></td>
-                            <td><img width="100px" src="data:image/png;base64,<?php echo base64_encode($row['imagen']);?>"></img></td>
-                            <td><a href="#">Modificar</a></td>
-                            <td><a href="#">Eliminar</a></td>       
-                        </tr>
+                                <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                
+                                    <strong>Registro insertado exitosamente</strong> You should check in on some of those fields below.
+                                </div>
+                    <?php      
+                            }
+                            else{
+                                ?>
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                
+                                    <strong>Error <?php echo mysqli_error($conexion);?></strong> You should check in on some of those fields below.
+                                </div>
                     <?php
+
+                            }    
+                        }
                     }
-                    ?>   
-                </tbody>
-        </table>
+                ?>
+                <form method="post" enctype="multipart/form-data">
+                    <div class="mb-3">
+                      <input type="file" class="form-control" name="imagen" id="" placeholder="" aria-describedby="fileHelpId">
+                      <div id="fileHelpId" class="form-text">Help text</div>
+                    </div>
 
+                    <div>
+                      <button type="submit" class="btn btn-primary" name="guardar">Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+  <header>
+    <!-- place navbar here -->
+  </header>
+  <main>
 
-    </center>
+  </main>
+  <footer>
+    <!-- place footer here -->
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
 </body>
+
 </html>
